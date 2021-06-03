6782580f5ad538bcd74be4d5b6e403563f3423c6
kelvinrandu <randukelvin@gmail.com>
2021-06-03 15:13:24 +0300

    animate header

--- framer/src/App.js
+++ framer/src/App.js
@@ -5,13 +5,14 @@ import {motion} from 'framer-motion';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <motion.header className="App-header" 
+      animate={{ rotateZ: 180, opacity:0.2, marginTop: 200}}>
        
         <motion.p animate={{ fontSize: 50, color: '#ff2994', x: 100, y: -100}}>
           Edit <code>src/App.js</code> and save to reload.
         </motion.p>
 
-      </header>
+      </motion.header>
     </div>
   );
 }
