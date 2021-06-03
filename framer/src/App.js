15b58c5d01d1b09cfb5674ee935afe2d17e9db76
kelvinrandu <randukelvin@gmail.com>
2021-06-03 15:55:24 +0300

    animate button

--- framer/src/App.js
+++ framer/src/App.js
@@ -3,17 +3,34 @@ import './App.css';
 import {motion} from 'framer-motion';
 
 function App() {
+  
+
   return (
-    <div className="App">
+    <>
+    <motion.div className="App"
+    initial={{ opacity: 0}}   
+    animate={{ rotate: 360, opacity: 1}}
+    transition={{ duration: 2 }} >
       <motion.header className="App-header" 
-      animate={{ rotateZ: 180, opacity:0.2, marginTop: 200}}>
+      animate={{  opacity:0.2, marginTop: 200, scale: 1.5}}>
        
-        <motion.p animate={{ fontSize: 50, color: '#ff2994', x: 100, y: -100}}>
+        <motion.p animate={{ fontSize: 50, color: '#c8efc8', x: 100, y: -100}}>
           Edit <code>src/App.js</code> and save to reload.
         </motion.p>
 
+
       </motion.header>
-    </div>
+    </motion.div>
+    <motion.div     
+    initial={{ x: '-100vw'}}   
+    animate={{ x: 0}}>
+      <button>add</button>
+      </motion.div>
+
+
+    </>
+    
+    
   );
 }
 
