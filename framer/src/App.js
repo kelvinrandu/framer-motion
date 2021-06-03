de0e91001018bec5e54c3a00bf24a050a4737804
kelvinrandu <randukelvin@gmail.com>
2021-06-03 15:08:15 +0300

    added animation to text

--- framer/src/App.js
+++ framer/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
        
-        <motion.p animate={{ fontSize: 50}}>
+        <motion.p animate={{ fontSize: 50, color: '#ff2994', x: 100, y: -100}}>
           Edit <code>src/App.js</code> and save to reload.
         </motion.p>
 
