3eb8065b3b71ae0a3394cfd822300121b2234e91
kelvinrandu <randukelvin@gmail.com>
2021-06-06 20:54:44 +0300

    finished to do and added animations

--- framer/src/App.js
+++ framer/src/App.js
@@ -1,10 +1,28 @@
-import "./App.css";
+import { React, useState, useEffect } from "react";
 import Todos from "./components/Todos";
-import { Heading, VStack, IconButton } from "@chakra-ui/react";
+import { Heading, VStack, IconButton, useColorMode } from "@chakra-ui/react";
 import AddTodo from "./components/AddTodo";
 import { FaSun, FaMoon } from "react-icons/fa";
+import { motion } from "framer-motion";
 
 function App() {
+  const [todos, setTodos] = useState(
+    () => JSON.parse(localStorage.getItem("todos")) || []
+  );
+  const { colorMode, toggleColorMode } = useColorMode();
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  function deleteTodo(id) {
+    const newTodos = todos.filter((todo) => {
+      return todo.id !== id;
+    });
+    setTodos(newTodos);
+  }
+  function addTodo(todo) {
+    setTodos([...todos, todo]);
+  }
   return (
     <>
       <VStack mt={3} p={4}>
@@ -15,20 +33,35 @@ function App() {
           variant="outline"
           aria-label="toggle colormode"
           alignSelf="flex-end"
-          icon={<FaMoon />}
+          onClick={toggleColorMode}
+          icon={colorMode === "light" ? <FaMoon /> : <FaSun />}
         />
-        <Heading
-          mb={8}
-          Size="2xl"
-          fontWeight="extrabold"
-          bgClip="text"
-          bgGradient="linear(to-r, green.200, pink.500)"
+        <motion.div
+          initial={{ y: -250 }}
+          animate={{ y: -10 }}
+          transition={{ delay: 0.2, type: "spring", stiffness: 120 }}
         >
-         
-          Todos extraordinaire
-        </Heading>
-        <Todos />
-        <AddTodo />
+          <Heading
+            mb={8}
+            Size="2xl"
+            fontWeight="extrabold"
+            bgClip="text"
+            bgGradient="linear(to-r, green.200, pink.500)"
+            initial={{ y: -250 }}
+            animate={{ y: -10 }}
+            transition={{ delay: 0.2, type: "spring", stiffness: 120 }}
+          >
+            Todos extraordinaire
+          </Heading>
+        </motion.div>
+        <Todos todos={todos} deleteTodo={deleteTodo} />
+        <motion.div
+          initial={{ x: "-100vw" }}
+          animate={{ x: 0 }}
+          transition={{ delay: 0.5 }}
+        >
+          <AddTodo addTodo={addTodo} />
+        </motion.div>
       </VStack>
     </>
   );
--- framer/src/index.js
+++ framer/src/index.js
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
+      <ColorModeScript initialColorMode="light"/>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
